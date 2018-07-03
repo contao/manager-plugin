ba00a4d70357c0641cfcb038da0c0cda05541458
Andreas Schempp <andreas.schempp@terminal42.ch>
2018-07-03 13:48:54 +0200

    Add the ApiPluginInterface (see #14)
    Description
    -----------
    
    The `ApiPluginInterface` allows a plugin to extend the Contao Manager API.
     1. by providing commands
     2. by extending the features list
    
    see https://github.com/contao/manager-bundle/pull/75 for the actual implementation
    
    Commits
    -------
    
    be92f08f Added ApiPluginInterface
    15898896 Remove the overrideApiFeatures method
    8cf81f09 Fix a phpDoc description

---
+++ src/Api/ApiPluginInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerPlugin\Api;
+
+interface ApiPluginInterface
+{
+    /**
+     * Returns the features this plugin can handle.
+     *
+     * @return array
+     */
+    public function getApiFeatures(): array;
+
+    /**
+     * Adds additional commands to the Manager API.
+     *
+     * @return array
+     */
+    public function getApiCommands(): array;
+}
