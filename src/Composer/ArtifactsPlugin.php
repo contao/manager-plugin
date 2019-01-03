376d110b77bce5338a57e42b08c36b48c234cc60
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-01-03 17:06:31 +0100

    Merge Composer config from contao-provider packages

--- src/Composer/ArtifactsPlugin.php
+++ src/Composer/ArtifactsPlugin.php
@@ -13,8 +13,14 @@ declare(strict_types=1);
 namespace Contao\ManagerPlugin\Composer;
 
 use Composer\Composer;
+use Composer\Config;
 use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
+use Composer\Package\PackageInterface;
+use Composer\Package\RootPackageInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Repository\ArtifactRepository;
+use Composer\Repository\RepositoryInterface;
 
 class ArtifactsPlugin implements PluginInterface
 {
@@ -23,17 +29,104 @@ class ArtifactsPlugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io): void
     {
-        $this->addArtifactRepository($composer);
-    }
-
-    private function addArtifactRepository(Composer $composer): void
-    {
         $packagesDir = $composer->getConfig()->get('data-dir').'/packages';
 
-        if (is_dir($packagesDir)) {
-            $composer->getRepositoryManager()->prependRepository(
-                $composer->getRepositoryManager()->createRepository('artifact', ['url' => $packagesDir])
-            );
+        if (!is_dir($packagesDir)) {
+            return;
         }
+
+        $repository = $this->addArtifactRepository($composer, $packagesDir);
+
+        $this->registerProviders($repository, $composer->getConfig(), $composer->getPackage());
+    }
+
+    private function addArtifactRepository(Composer $composer, string $repositoryUrl): RepositoryInterface
+    {
+        $repository = $composer->getRepositoryManager()->createRepository('artifact', ['url' => $repositoryUrl]);
+
+        $composer->getRepositoryManager()->prependRepository($repository);
+
+        return $repository;
+    }
+
+    private function registerProviders(RepositoryInterface $repository, Config $config, RootPackageInterface $rootPackage): void
+    {
+        $requires = $rootPackage->getRequires();
+
+        foreach ($repository->getPackages() as $package) {
+            if ('contao-provider' !== $package->getType() || !\array_key_exists($package->getName(), $requires)) {
+                continue;
+            }
+
+            $data = $this->getComposerInformation($package);
+
+            if (null !== $data) {
+                $config->merge($data);
+            }
+        }
+    }
+
+    /**
+     * @see ArtifactRepository::getComposerInformation()
+     */
+    private function getComposerInformation(PackageInterface $package): ?array
+    {
+        $zip = new \ZipArchive();
+        $zip->open($package->getDistUrl());
+
+        if (0 == $zip->numFiles) {
+            return null;
+        }
+
+        $foundFileIndex = $this->locateFile($zip, 'composer.json');
+        if (false === $foundFileIndex) {
+            return null;
+        }
+
+        $configurationFileName = $zip->getNameIndex($foundFileIndex);
+
+        $composerFile = "zip://{$package->getDistUrl()}#$configurationFileName";
+        $json = file_get_contents($composerFile);
+
+        return JsonFile::parseJson($json, $composerFile);
+    }
+
+    /**
+     * @see ArtifactRepository::locateFile()
+     */
+    private function locateFile(\ZipArchive $zip, string $filename)
+    {
+        $indexOfShortestMatch = false;
+        $lengthOfShortestMatch = -1;
+
+        for ($i = 0; $i < $zip->numFiles; $i++) {
+            $stat = $zip->statIndex($i);
+            if (strcmp(basename($stat['name']), $filename) === 0) {
+                $directoryName = dirname($stat['name']);
+                if ($directoryName === '.') {
+                    //if composer.json is in root directory
+                    //it has to be the one to use.
+                    return $i;
+                }
+
+                if (strpos($directoryName, '\\') !== false ||
+                    strpos($directoryName, '/') !== false) {
+                    //composer.json files below first directory are rejected
+                    continue;
+                }
+
+                $length = strlen($stat['name']);
+                if ($indexOfShortestMatch === false || $length < $lengthOfShortestMatch) {
+                    //Check it's not a directory.
+                    $contents = $zip->getFromIndex($i);
+                    if ($contents !== false) {
+                        $indexOfShortestMatch = $i;
+                        $lengthOfShortestMatch = $length;
+                    }
+                }
+            }
+        }
+
+        return $indexOfShortestMatch;
     }
 }
--- tests/Composer/ArtifactsPluginTest.php
+++ tests/Composer/ArtifactsPluginTest.php
@@ -15,6 +15,8 @@ namespace Contao\ManagerPlugin\Tests\Composer;
 use Composer\Composer;
 use Composer\Config;
 use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Package\RootPackageInterface;
 use Composer\Repository\RepositoryInterface;
 use Composer\Repository\RepositoryManager;
 use Contao\ManagerPlugin\Composer\ArtifactsPlugin;
@@ -24,14 +26,18 @@ class ArtifactsPluginTest extends TestCase
 {
     public function testAddsArtifactsRepository()
     {
-        $io = $this->createMock(IOInterface::class);
         $repository = $this->createMock(RepositoryInterface::class);
+        $repository
+            ->method('getPackages')
+            ->willReturn([])
+        ;
+
         $repositoryManager = $this->createMock(RepositoryManager::class);
 
         $repositoryManager
             ->expects($this->once())
             ->method('createRepository')
-            ->with('artifact', ['url' => __DIR__.'/../Fixtures/Composer/test-data/packages'])
+            ->with('artifact', ['url' => __DIR__.'/../Fixtures/Composer/artifact-data/packages'])
             ->willReturn($repository)
         ;
 
@@ -41,22 +47,84 @@ class ArtifactsPluginTest extends TestCase
             ->with($repository)
         ;
 
-        $composer = $this->mockComposerWithDataDir(__DIR__.'/../Fixtures/Composer/test-data');
-
-        $composer
-            ->expects($this->atLeastOnce())
-            ->method('getRepositoryManager')
-            ->willReturn($repositoryManager)
-        ;
+        $config = $this->mockConfigWithDataDir(__DIR__.'/../Fixtures/Composer/artifact-data');
+        $composer = $this->mockComposerWithDataDir($config, $repositoryManager);
 
         $plugin = new ArtifactsPlugin();
-        $plugin->activate($composer, $io);
+        $plugin->activate($composer, $this->createMock(IOInterface::class));
+    }
+
+    public function testRegistersProviders()
+    {
+        $config = $this->mockConfigWithDataDir(__DIR__.'/../Fixtures/Composer/provider-data');
+
+        $config
+            ->expects($this->once())
+            ->method('merge')
+        ;
+
+        $composer = $this->mockComposerWithDataDir(
+            $config,
+            null,
+            [$this->mockPackage('foo/bar', 'contao-provider', __DIR__.'/../Fixtures/Composer/provider-data/packages/foo-bar-1.0.0.zip')],
+            ['foo/bar' => '*']
+        );
+
+        $plugin = new ArtifactsPlugin();
+        $plugin->activate($composer, $this->createMock(IOInterface::class));
+    }
+
+    public function testDoesNotRegisterNotRequiredProviders()
+    {
+        $config = $this->mockConfigWithDataDir(__DIR__.'/../Fixtures/Composer/provider-data');
+
+        $config
+            ->expects($this->never())
+            ->method('merge')
+        ;
+
+        $composer = $this->mockComposerWithDataDir(
+            $config,
+            null,
+            [$this->mockPackage('foo/bar', 'contao-provider')],
+            []
+        );
+
+        $plugin = new ArtifactsPlugin();
+        $plugin->activate($composer, $this->createMock(IOInterface::class));
+    }
+
+    public function testDoesNotRegisterNonProviderPackages()
+    {
+        $config = $this->mockConfigWithDataDir(__DIR__.'/../Fixtures/Composer/artifact-data');
+
+        $config
+            ->expects($this->never())
+            ->method('merge')
+        ;
+
+        $composer = $this->mockComposerWithDataDir(
+            $config,
+            null,
+            [$this->mockPackage('foo/bar', 'contao-bundle')],
+            ['foo/bar' => '*']
+        );
+
+        $plugin = new ArtifactsPlugin();
+        $plugin->activate($composer, $this->createMock(IOInterface::class));
     }
 
     public function testDoesNothingWithoutPackagesDir()
     {
         $io = $this->createMock(IOInterface::class);
-        $composer = $this->mockComposerWithDataDir(__DIR__.'/../Fixtures/Composer/null-data');
+        $config = $this->mockConfigWithDataDir(__DIR__.'/../Fixtures/Composer/null-data');
+
+        $composer = $this->createMock(Composer::class);
+
+        $composer
+            ->method('getConfig')
+            ->willReturn($config)
+        ;
 
         $composer
             ->expects($this->never())
@@ -70,23 +138,84 @@ class ArtifactsPluginTest extends TestCase
     /**
      * @return Composer|\PHPUnit_Framework_MockObject_MockObject
      */
-    private function mockComposerWithDataDir(string $dataDir): Composer
+    private function mockComposerWithDataDir($config, $repositoryManager = null, array $packages = [], $requires = []): Composer
+    {
+        $rootPackage = $this->createMock(RootPackageInterface::class);
+        $rootPackage
+            ->expects(empty($requires) ? $this->any() : $this->atLeastOnce())
+            ->method('getRequires')
+            ->willReturn($requires)
+        ;
+
+        if (null === $repositoryManager) {
+            $repository = $this->createMock(RepositoryInterface::class);
+            $repository
+                ->method('getPackages')
+                ->willReturn($packages)
+            ;
+
+            $repositoryManager = $this->createMock(RepositoryManager::class);
+            $repositoryManager
+                ->method('createRepository')
+                ->willReturn($repository);
+        }
+
+        $composer = $this->createMock(Composer::class);
+
+        $composer
+            ->method('getConfig')
+            ->willReturn($config)
+        ;
+
+        $composer
+            ->expects($this->atLeastOnce())
+            ->method('getRepositoryManager')
+            ->willReturn($repositoryManager)
+        ;
+
+        $composer
+            ->method('getPackage')
+            ->willReturn($rootPackage)
+        ;
+
+        return $composer;
+    }
+
+    private function mockConfigWithDataDir(string $dataDir)
     {
         $config = $this->createMock(Config::class);
+
         $config
-            ->expects($this->once())
             ->method('get')
             ->with('data-dir')
             ->willReturn($dataDir)
         ;
 
-        $composer = $this->createMock(Composer::class);
-        $composer
-            ->expects($this->once())
-            ->method('getConfig')
-            ->willReturn($config)
+        return $config;
+    }
+
+    private function mockPackage(string $name, string $type, string $distUrl = null)
+    {
+        $package = $this->createMock(PackageInterface::class);
+
+        $package
+            ->expects('contao-provider' === $type ? $this->once() : $this->never())
+            ->method('getName')
+            ->willReturn($name)
         ;
 
-        return $composer;
+        $package
+            ->expects($this->once())
+            ->method('getType')
+            ->willReturn($type)
+        ;
+
+        $package
+            ->expects($distUrl ? $this->exactly(2) : $this->never())
+            ->method('getDistUrl')
+            ->willReturn($distUrl)
+        ;
+
+        return $package;
     }
 }
