549c46a4f06bb4e0565eae9647c96fda6a545d02
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-01-08 16:08:40 +0100

    Autoload the App namespace if none is configured

---
+++ src/Composer/AppAutoloadPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerPlugin\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class AppAutoloadPlugin implements PluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $rootPackage = $composer->getPackage();
+
+        $autoload = $rootPackage->getAutoload();
+
+        if (empty($autoload)) {
+            $rootPackage->setAutoload(['psr-4' => ['App\\' => 'src/']]);
+        }
+    }
+}
