b25aee48ce14948b103e8485d287b918bd3029b6
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-01-24 19:14:14 +0100

    Autoload the App namespace if none is configured (#22)

---
+++ tests/Composer/AppAutoloadPluginTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerPlugin\Tests\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Package\RootPackageInterface;
+use Contao\ManagerPlugin\Composer\AppAutoloadPlugin;
+use PHPUnit\Framework\TestCase;
+
+class AppAutoloadPluginTest extends TestCase
+{
+    public function testAddsTheAppNamespaceIfNoneIsDefined()
+    {
+        $package = $this->createMock(RootPackageInterface::class);
+        $package
+            ->expects($this->once())
+            ->method('getAutoload')
+            ->willReturn([])
+        ;
+
+        $package
+            ->expects($this->once())
+            ->method('setAutoload')
+            ->with(['psr-4' => ['App\\' => 'src/']])
+        ;
+
+        $composer = $this->createMock(Composer::class);
+        $composer
+            ->expects($this->once())
+            ->method('getPackage')
+            ->willReturn($package)
+        ;
+
+        $io = $this->createMock(IOInterface::class);
+        $io
+            ->expects($this->never())
+            ->method($this->anything())
+        ;
+
+        (new AppAutoloadPlugin())->activate($composer, $io);
+    }
+
+    public function testDoesNotAddTheAppNamespaceIfOneIsDefined()
+    {
+        $package = $this->createMock(RootPackageInterface::class);
+        $package
+            ->expects($this->once())
+            ->method('getAutoload')
+            ->willReturn(['psr-4' => ['AppBundle\\' => 'src/']])
+        ;
+
+        $package
+            ->expects($this->never())
+            ->method('setAutoload')
+        ;
+
+        $composer = $this->createMock(Composer::class);
+        $composer
+            ->expects($this->once())
+            ->method('getPackage')
+            ->willReturn($package)
+        ;
+
+        $io = $this->createMock(IOInterface::class);
+        $io
+            ->expects($this->never())
+            ->method($this->anything())
+        ;
+
+        (new AppAutoloadPlugin())->activate($composer, $io);
+    }
+}
